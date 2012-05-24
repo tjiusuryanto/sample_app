dadb5c1e2cc1e81cc6ef04887e47abfb05a28ee0
Tjiu Suryanto <tjiu.suryanto@gmail.com>
2012-05-24 19:31:16 +0700

    Listing 9.6

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -34,8 +34,11 @@ describe "Authentication" do
         click_button "Sign in"
       end
 
+      before { sign_in user }
+
       it { should have_selector('title', text: user.name) }
-      it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Profile',  href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
     end
