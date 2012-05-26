0bf9197c44a0a7f30dd09b36622bb807c5f61cad
Tjiu Suryanto <tjiu.suryanto@gmail.com>
2012-05-27 00:29:16 +0700

    Listing 9.18

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  # Fprce sign in when attempting edit or update
+  before_filter :signed_in_user, only: [:edit, :update]
+
+  before_filter :correct_user,   only: [:edit, :update]
 	
   def show
     @user = User.find(params[:id])
@@ -20,15 +25,29 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
+    # Covered by the method correct_user
+    # @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
-      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
     else
       render 'edit'
     end
+  end
+
+  private
+
+  def signed_in_user
+      redirect_to signin_path, notice: "Please sign in." unless signed_in?
+  end
+
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
   end
 end
