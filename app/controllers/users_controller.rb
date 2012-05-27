e801f4b25a93ed7d1afe3f139e4b4b512ef165c5
Tjiu Suryanto <tjiu.suryanto@gmail.com>
2012-05-27 23:28:31 +0700

    Listing 9.28

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
 
   # Fprce sign in when attempting edit or update
-  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update]
 
   before_filter :correct_user,   only: [:edit, :update]
 	
   def show
     @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
   end
 
   def new
@@ -43,7 +47,10 @@ class UsersController < ApplicationController
   private
 
   def signed_in_user
-      redirect_to signin_path, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in."
+      end
   end
 
   def correct_user
