d33b1ab2ce7b11ce6723c4d4f4fb6e8eaa8a29e3
Tjiu Suryanto <tjiu.suryanto@gmail.com>
2012-06-14 13:18:54 +0700

    Listing 9.48

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
 
   # Fprce sign in when attempting edit or update
-  before_filter :signed_in_user, only: [:index, :edit, :update]
-
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
 	
   def show
     @user = User.find(params[:id])
@@ -45,6 +45,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+
   private
 
   def signed_in_user
@@ -58,4 +64,8 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
   end
+
+  def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
