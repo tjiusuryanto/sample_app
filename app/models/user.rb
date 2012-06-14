d6f8e5d05c7fc070a6be932849b21501d3335fff
Tjiu Suryanto <tjiu.suryanto@gmail.com>
2012-06-14 21:50:15 +0700

    Listing 11.10

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
 
   before_save { self.email.downcase! }
   before_save :create_remember_token
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,8 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
 
   it { should be_valid }
   it { should_not be_admin }
